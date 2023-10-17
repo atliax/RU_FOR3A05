3cc60949d92b0437cb51362518f30c4c05e1e13b
Atli Axfjörð <atliax@gmail.com>
2023-10-17 14:51:15 +0000

    Æfingaverkefni 2

---
+++ 5_Aefingaverkefni2_b.py
@@ -0,0 +1,8 @@
+import math
+
+radius = 20
+flatarmal_hrings = radius**2 * math.pi
+ummal_hrings = 2 * radius * math.pi
+
+print("Flatarmál hrings með radíus "+str(radius)+" er: "+str(flatarmal_hrings))
+print("Ummál hrings með radíus "+str(radius)+" er: "+str(ummal_hrings))
---
+++ 6_Aefingaverkefni2_c.py
@@ -0,0 +1,7 @@
+lengd = 5
+breidd = 10
+flatarmal = lengd*breidd
+ummal = 2*lengd + 2*breidd
+
+print("Flatarmál ferhyrnings með lengd "+str(lengd)+" og breidd "+str(breidd)+" er: "+str(flatarmal))
+print("Ummál ferhyrnings með lengd "+str(lengd)+" og breidd "+str(breidd)+" er: "+str(ummal))
