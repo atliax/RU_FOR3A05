b742fdf65ed7b3755d61662be960bcc26a778785
Atli Axfjörð <atliax@gmail.com>
2023-10-19 20:11:39 +0000

    Æfingaverkefni 6

---
+++ 23_Aefingaverkefni6.py
@@ -0,0 +1,37 @@
+#dæmi 1
+nidurst = 2 + 3 * 5
+print(nidurst)
+#ég býst við: 17
+#rétt
+
+#dæmi 2
+nidurst = 2 + 3 * 5 % 4
+print(nidurst)
+#ég býst við: 5
+#rétt
+
+#dæmi 3
+nidurst = 5 / 4
+print(nidurst)
+#ég býst við: 1.25
+#rétt
+
+#dæmi 4
+nidurst = 5 // 4
+print(nidurst)
+#ég býst við: 1
+#rétt
+
+#dæmi 5
+nidurst = 5
+nidurst += 5 / 4.0
+print(nidurst)
+#ég býst við: 6.25
+#rétt
+
+#dæmi 6
+nidurst = 5
+nidurst %= 2
+print(nidurst)
+#ég býst við: 1
+#rétt
