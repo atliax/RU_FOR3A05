59b137d7b8bd4e1b98366332700c0228cb0723b0
Atli Axfjörð <atliax@gmail.com>
2023-10-26 00:24:26 +0000

    Æfingaverkefni 9 leiðrétting

--- 46_Aefingaverkefni9_b_5.py
+++ 46_Aefingaverkefni9_b_5.py
@@ -1,6 +1,6 @@
 for i in range(1,9):
     for j in range(1,9):
-        if((i+j) % 2 == 0):
+        if( (i+j) % 2 == 0 ):
             print("H ", end="")
         else:
             print("S ", end="")
