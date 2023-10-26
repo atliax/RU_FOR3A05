8bdf6b645ebe5c796b82399f97a1d767ec14bb31
Atli Axfjörð <atliax@gmail.com>
2023-10-26 02:57:52 +0000

    Tilraun

---
+++ 41_Aefingaverkefni9_a.py
@@ -0,0 +1,40 @@
+'''
+Bls. 141 (Basic Control Check):
+
+1a:
+    4
+
+1b:
+    0
+
+1c:
+    string
+
+1d:
+    assignment operator
+
+1e:
+    to mark the start of the while loop code block
+
+Bls. 151 (Loop Control Check):
+
+1a i:
+    
+
+1a ii:
+
+1a iii:
+
+1a iv:
+
+1b i:
+
+1b ii:
+
+1b iii:
+
+1b iv:
+
+1c:
+
+'''
---
+++ 47_Experiment1.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Prufa.")
+greeting.pack()
+
+window.mainloop()
