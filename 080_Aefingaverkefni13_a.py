3b6e90fa676ee5a5945cbcb8ecb9c8f9378fa519
Atli Axfjörð <atliax@gmail.com>
2023-11-06 11:01:28 +0000

    Æfingaverkefni 13 A

---
+++ 080_Aefingaverkefni13_a.py
@@ -0,0 +1,16 @@
+"""
+Slicing check, bls. 230
+
+1a: "bbcc"
+1b: "abb"
+1c: "abc"
+1d: "aabb"
+1e: "a b"
+
+String comparison check, bls. 236
+1a: ''
+1b: 'cb'
+1c: 'cc'
+1d: loopan mun keyra endalaust
+1e: Index error þegar hann reynir my_str[3]
+"""
