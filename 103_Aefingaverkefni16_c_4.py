794fd05776f011252159fae3f27c6afcef8602e4
Atli Axfjörð <atliax@gmail.com>
2023-11-17 10:22:28 +0000

    Æfingaverkefni 16 C 4 með  annarri aðferð

--- 103_Aefingaverkefni16_c_4.py
+++ 103_Aefingaverkefni16_c_4.py
@@ -1,14 +1,16 @@
 x = 3
 y = 3
 
-leikbord = [[0] * y for _ in range(x)]
+leikbord = []
 
 for i in range(0,x):
+    tmp = []
     for j in range(0,y):
-        if (i+j) % 2 == 0:
-            leikbord[i][j] = "X"
+        if(i+j) % 2 == 0:
+            tmp.append("X")
         else:
-            leikbord[i][j] = "O"
+            tmp.append("O")
+    leikbord.append(tmp)
 
 for i in range(0,x):
     for j in range(0,y):
