ac5e7e14aa68d9308b9cb7ea42fd15aee18713d2
Atli Axfjörð <atliax@gmail.com>
2023-11-24 10:38:58 +0000

    Æfingaverkefni 22 C aðeins öðruvísi

--- 127_Aefingaverkefni22_c.py
+++ 127_Aefingaverkefni22_c.py
@@ -15,6 +15,19 @@ for ord in ordin:
     else:
         talning[ord] = 1
 
-algengasta_ord = max(talning.items(), key=lambda x:x[1])
-print(f"Algengasta orðið: \"{algengasta_ord[0]}\"")
-print(f"það kom fyrir svona oft: {algengasta_ord[1]}")
+init = False
+algengast = ""
+
+for key, value in talning.items():
+    if not init:
+        algengast = key
+        init = True
+        continue
+    if talning[algengast] < value:
+        algengast = key
+
+algengasta_talning = talning[algengast]
+
+print(f"Heildarfjöldi orða: {len(ordin)}")
+print(f"Algengasta orðið: \"{algengast}\"")
+print(f"það kom fyrir svona oft: {algengasta_talning}")
