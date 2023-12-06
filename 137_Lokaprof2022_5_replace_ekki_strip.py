8632741b8ece0dcee4a8c6f0885ebc50b502e991
Atli Axfjörð <atliax@gmail.com>
2023-12-06 18:20:44 +0000

    Lokapróf 2022 Dæmi 5, notar replace() ekki strip()

---
+++ 137_Lokaprof2022_5_replace_ekki_strip.py
@@ -0,0 +1,17 @@
+filename = "JorgeLuisBorgesQuote.txt"
+
+oll_ordin = []
+ord_talning = {}
+
+with open(filename, "r") as file:
+    raw_innihald = file.read().split()
+
+for ord in raw_innihald:
+    oll_ordin.append(ord.replace("“","").replace("”","").replace(",","").replace(".","").lower())
+
+einstok_ord = set(oll_ordin)
+fjoldi_einstakra_orda = len(einstok_ord)
+heildarfjoldi_orda = len(oll_ordin)
+
+print(f"Heildarfjöldi orða: {heildarfjoldi_orda}")
+print(f"Fjöldi einstakra orða: {fjoldi_einstakra_orda}")
