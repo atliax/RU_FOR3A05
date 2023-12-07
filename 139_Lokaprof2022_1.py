5811b4ac8e9e3ae210f86ab80a0ee02af019f3af
Atli Axfjörð <atliax@gmail.com>
2023-12-07 10:26:37 +0000

    Lokapróf 2022 Dæmi 1 leiðrétting

--- 139_Lokaprof2022_1.py
+++ 139_Lokaprof2022_1.py
@@ -1,3 +1,13 @@
+def saekja_heiltolu(strengur):
+    heiltala_komin = False
+    while not heiltala_komin:
+        try:
+            tala = int(input(strengur))
+            heiltala_komin = True
+        except ValueError:
+            print("Þetta skilst ekki sem heiltala, reyndu aftur...")
+    return tala
+
 PI = 3.14
 
 input_komid = False
@@ -8,11 +18,20 @@ while not input_komid:
     except ValueError:
         print("Þetta var ekki heilbrigð tala hjá þér...")
 
-flatarmal_hrings = radius*radius*PI
-ummal_hrings = 2*radius*PI
-rummal_kulu = (4.0*PI*(radius**3)) / 3.0
+val = 0
+print("1. Flatarmál hrings")
+print("2. Ummál hrings")
+print("3. Rúmmál kúlu")
+print("4. Hætta")
+while val < 1 or val > 4:
+    val = saekja_heiltolu("Veldu 1-4: ")
 
-print(f"Ef gefinn er radíusinn {radius}, þá reiknast eftirfarandi:")
-print(f"Flatarmál hrings: {flatarmal_hrings}")
-print(f"Ummál hrings: {ummal_hrings}")
-print(f"Rúmmál kúlu: {rummal_kulu}")
+if(val == 1):
+    flatarmal_hrings = radius*radius*PI
+    print(f"Flatarmál hrings: {flatarmal_hrings}")
+elif(val == 2):
+    ummal_hrings = 2*radius*PI
+    print(f"Ummál hrings: {ummal_hrings}")
+elif(val == 3):
+    rummal_kulu = (4.0*PI*(radius**3)) / 3.0
+    print(f"Rúmmál kúlu: {rummal_kulu}")
